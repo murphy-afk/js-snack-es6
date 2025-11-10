e62067fad26f62343b3b5d3f303384516802ce2f
murphy-afk <bardarenoemi@gmail.com>
2025-11-10 14:42:03 +0100

    first commit

---
+++ script/script.js
@@ -0,0 +1,25 @@
+// Snack 1
+// Creare un array di oggetti:
+// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+// Stampare a schermo oppure in console la bici con peso minore.
+
+// Snack2
+// Creare un array di oggetti di squadre di calcio.
+// Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+// Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+// Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
+// Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti
+//  e stampiamo tutto in console.
+
+// Snack 3 (Bonus)
+// Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b). 
+// La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa
+//  tra i due numeri. Non usare i metodi di array di JS 
+
+const bicycles = [
+  { name: "Pinarello Dogma F", weight: 6.8 },
+  { name: "Specialized Tarmac SL8", weight: 6.6 },
+  { name: "Trek Émonda SLR", weight: 6.7 },
+  { name: "Canyon Aeroad CF SLX", weight: 7.1 },
+  { name: "Bianchi Oltre RC", weight: 6.9 }
+];
