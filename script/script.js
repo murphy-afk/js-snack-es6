0e5c78aadca48d7c6f997b0bbfe4ec1e6cb0cc55
murphy-afk <bardarenoemi@gmail.com>
2025-11-10 15:01:39 +0100

    feat: snack 1

--- script/script.js
+++ script/script.js
@@ -17,9 +17,22 @@
 //  tra i due numeri. Non usare i metodi di array di JS 
 
 const bicycles = [
-  { name: "Pinarello Dogma F", weight: 6.8 },
-  { name: "Specialized Tarmac SL8", weight: 6.6 },
-  { name: "Trek Émonda SLR", weight: 6.7 },
+  { name: "Pinarello Dogma F", weight: 6.7 },
+  { name: "Trek Émonda SLR", weight: 6.8 },
+  { name: "Specialized Tarmac SL8", weight: 6.9 },
   { name: "Canyon Aeroad CF SLX", weight: 7.1 },
-  { name: "Bianchi Oltre RC", weight: 6.9 }
+  { name: "Bianchi Oltre RC", weight: 6.5 }
 ];
+
+let lightestWeight = bicycles[0].weight;
+let lightestBike;
+for (let i = 1; i < bicycles.length; i++) {
+  let curBike = bicycles[i];
+  if (lightestWeight > curBike.weight) {
+    lightestWeight = curBike.weight;
+    lightestBike = curBike;
+  }
+};
+console.log(lightestBike);
+
+
