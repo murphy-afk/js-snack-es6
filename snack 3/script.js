6340f1514e8762d377d15e612d530e11223453e4
murphy-afk <bardarenoemi@gmail.com>
2025-11-10 16:32:43 +0100

    feat: snack 3 function

--- snack 3/script.js
+++ snack 3/script.js
@@ -1,16 +1,31 @@
 // Snack 3 (Bonus)
 // Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b). 
-// I DUE NUMERI INDICNAO DUE INDICI 
+// I DUE NUMERI INDICANO DUE INDICI 
 // La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa
 //  tra i due numeri. Non usare i metodi di array di JS 
 
 const ex = [1, 2, 3, 4, 5, 6, 7];
-const newEx = [];
 const num1 = 2;
 const num2 = 5;
-for (let i = 0; i < ex.length; i++) {
-  if (i >= num1 && i < num2) {
-    newEx.push(ex[i])
+// const newEx = [];
+// for (let i = 0; i < ex.length; i++) {
+//   if (i >= num1 && i < num2) {
+//     newEx.push(ex[i])
+//   }
+// }
+// console.log(newEx);
+
+const filterByIndex = (array, first, last) => {
+  if (last > first) {
+  result = [];
+  for (let i = 0; i < array.length; i++) {
+    if (i >= first && i <= last) result.push(array[i]);
   }
 }
-console.log(newEx);
+else {
+  alert("il primo numero deve essere inferiore al secondo");
+}
+  return result
+};
+
+console.log(filterByIndex(ex, num1, num2));
